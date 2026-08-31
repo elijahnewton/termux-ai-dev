1532fac1f7624ec38f5c708208cc389cce9386db
ELIJAH MUSIITWA <65656407+elijahnewton@users.noreply.github.com>
2026-08-31 19:34:36 +0300

    Update ui.go

--- ui.go
+++ ui.go
@@ -1,39 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
+    "fmt"
+    "os"
+    "sync"
+    "time"
 )
 
 type SimpleSpinner struct {
-	stop chan struct{}
+    stop chan struct{}
+    done chan struct{}
+    once sync.Once
 }
 
 func NewSpinner(status string) *SimpleSpinner {
-	s := &SimpleSpinner{stop: make(chan struct{})}
-	go s.run(status)
-	return s
+    s := &SimpleSpinner{
+        stop: make(chan struct{}),
+        done: make(chan struct{}),
+    }
+    go s.run(status)
+    return s
 }
 
 func (s *SimpleSpinner) run(status string) {
-	frames := []string{"|", "/", "-", "\\"}
-	i := 0
-	ticker := time.NewTicker(250 * time.Millisecond)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			fmt.Fprintf(os.Stderr, "[K[1;33m%s[0m %s", frames[i], status)
-			i = (i + 1) % len(frames)
-		case <-s.stop:
-			fmt.Fprintf(os.Stderr, "[K")
-			return
-		}
-	}
+    defer close(s.done)
+    frames := []string{"|", "/", "-", "\\"}
+    i := 0
+    ticker := time.NewTicker(250 * time.Millisecond)
+    defer ticker.Stop()
+    for {
+        select {
+        case <-ticker.C:
+            fmt.Fprintf(os.Stderr, "\r\033[K\033[1;33m%s\033[0m %s", frames[i], status)
+            i = (i + 1) % len(frames)
+        case <-s.stop:
+            fmt.Fprint(os.Stderr, "\r\033[K")
+            return
+        }
+    }
 }
 
+// Stop halts the spinner and waits for the goroutine to exit, so no stray
+// frame garbles output printed afterwards. Safe to call more than once.
 func (s *SimpleSpinner) Stop() {
-	close(s.stop)
-	time.Sleep(60 * time.Millisecond)
+    s.once.Do(func() { close(s.stop) })
+    <-s.done
 }
